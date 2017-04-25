1c437f679e46e0a20438013e9f824bf9c3b9c7f0
Przemek <przemyslawtluszcz@gmail.com>
2017-04-25 22:38:04 +0000

    Update Check for Palindromes.js

--- Check for Palindromes.js
+++ Check for Palindromes.js
@@ -1,6 +1,6 @@
 function palindrome(str) {
   var newStr = str.replace(/[_\W]+/g, "").toLowerCase();
-  var halfStr = newStr.split("").reverse().join("").split(newStr.length / 2);
+  var halfStr = newStr.split("").reverse().join("");
 
   return newStr.includes(halfStr);
 }
