e39a1b55fe26c8c6b84dc773889b098a1aaabe3f
Przemek <przemyslawtluszcz@gmail.com>
2017-04-26 11:17:46 +0000

    Update Confirm the Ending.js

--- Confirm the Ending.js
+++ Confirm the Ending.js
@@ -1,9 +1,5 @@
 function confirmEnding(str, target) {
-  if (str.substring(str.length - target.length) == target) {
-    return true;
-  } else {
-    return false;
-  }
+  return str.includes(target, str.length - target.length);
 }
 
-confirmEnding("Bastian", "n");
+confirmEnding("Bastin", "n");
