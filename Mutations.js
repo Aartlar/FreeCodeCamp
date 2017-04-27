afc314fda488a1809ea0077b92f18b145a582650
Przemek <przemyslawtluszcz@gmail.com>
2017-04-27 12:14:42 +0000

    Update Mutations.js

--- Mutations.js
+++ Mutations.js
@@ -1,14 +1,9 @@
 function mutation(arr) {
   var newArr = arr[0].toLowerCase();
-  var serached = arr[1].toLowerCase();
-
-  for (var i = 0; i < arr[1].length; i++) {
-    var ex = 0;
-    if (newArr.indexOf(serached[i]) >= 0) {
-      continue;
-    } else return false;
-  }
-  return true;
+  var serached = arr[1].toLowerCase().split("");
+  return serached.every(function(element){
+    return newArr.indexOf(element) >= 0;
+  });
 }
 
-mutation(["hello", "sey"]);
+mutation(["Hello", "he"]);
