63accfbfed8140b62d0ef34694ec75f3741c4c7f
Przemek <przemyslawtluszcz@gmail.com>
2017-04-29 01:06:46 +0000

    Update Where do I belong.js

--- Where do I belong.js
+++ Where do I belong.js
@@ -1,19 +1,11 @@
 function getIndexToIns(arr, num) {
-  arr.sort(function(a, b) {
+  var sortedArr = arr;
+  sortedArr.push(num);
+  sortedArr.sort(function(a, b) {
     return a - b;
   });
-
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] >= num) {
-      return i;
-    } else if (i === arr.length - 1 && arr[i] < num) {
-      return i + 1;
-    } else {
-      continue;
-    }
-  }
-
-
+  return sortedArr.indexOf(num);
+  
 }
 
 getIndexToIns([5, 3, 20, 3], 5);
