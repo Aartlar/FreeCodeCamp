fe88330738fe3620a24262574a5c7443abb5491b
Przemek <przemyslawtluszcz@gmail.com>
2017-04-29 00:39:33 +0000

    Update Truncate a string.js

--- Truncate a string.js
+++ Truncate a string.js
@@ -13,3 +13,22 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 3);
+
+// same ternary
+
+function truncateString(str, num) {
+  return num <= 3? str.substr(0, num) + "...": str.length > num? str.substr(0, num -3) + "..." : str;
+}
+
+truncateString("Peter Piper picked a peck of pickled peppers", 14);
+
+// a jako że wspominałeś o nieczytelności 2 warunków w ternary to dodaje wersje bardziej czytelną
+
+function truncateString(str, num) {
+  if (num <= 3) {
+    return str.substr(0, num) + "...";
+  } 
+  return str.length > num? str.substr(0, num -3) + "..." : str;
+}
+
+truncateString("Peter Piper picked a peck of pickled peppers", 14);
