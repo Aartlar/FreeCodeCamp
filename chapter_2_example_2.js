22fa5eaaa3cad6cdba10cca66aae5ccb5bec7018
Przemek <przemyslawtluszcz@gmail.com>
2017-04-29 13:41:26 +0000

    Update chapter_2_example_2.js

--- chapter_2_example_2.js
+++ chapter_2_example_2.js
@@ -1,18 +1,16 @@
+// kompletnie niewiem jak to zrobić w 1 pętli.
 var size = 8;
-var string = "";
+var board = "";
 
-for (var i = 1; i <= size; i++) {
-  for (var j = 1; j <= size; j++) {
-    if (i % 2 !== 0 && j % 2 === 0) {
-      string += " ";
-    } else if (i % 2 !== 0 && j % 2 !== 0) {
-      string += "#";
-    } else if (i % 2 === 0 && j % 2 === 0) {
-      string += "#";
-    } else if (i % 2 === 0 && j % 2 !== 0) {
-      string += " ";
+for (var i = 0; y < size; i++) {
+  for (var j = 0; x < size; j++) {
+    if ((i + j) % 2 === 0) {
+      board += " ";
+    } else {
+      board += "#";
     }
   }
-  string += "\n";
+  board += "\n";
 }
-console.log(string);
+
+console.log(board);
