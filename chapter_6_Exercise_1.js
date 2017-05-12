1aa7f5089112a3ba4454075cf76900fd01f03477
Przemek <przemyslawtluszcz@gmail.com>
2017-05-12 03:45:01 +0000

    Add new file

---
+++ chapter_6_Exercise_1.js
@@ -0,0 +1,16 @@
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+  this.x + other.x;
+  this.y + other.y;
+}
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
